c1802cb174738cedc0a49808f63eceadd810a352
Ragullura <ragullura.005@gmail.com>
2024-06-01 16:37:33 +0530

    complete pricing section

--- client/src/components/Pricing.jsx
+++ client/src/components/Pricing.jsx
@@ -28,7 +28,7 @@ export default function Pricing() {
                                     </li>
                                 ))}
                             </ul>
-                            <a href="#" className='inline-flex justify-center items-center text-center w-full h-12 p-5 mt-20 tracking-tight text-xl hover:bg-gradient-to-r hover:from-red-500 hover:to-red-800 border border-orange-900 rounded-lg transition duration-200'>Subscribe</a>
+                            <a href="#" className='inline-flex justify-center items-center text-center w-full h-12 p-5 mt-20 tracking-tight text-xl hover:bg-gradient-to-r hover:from-red-500 hover:to-red-800 border border-orange-900 rounded-lg transition duration-200 '>Subscribe</a>
                     </div>
                 </div>
             ))}
